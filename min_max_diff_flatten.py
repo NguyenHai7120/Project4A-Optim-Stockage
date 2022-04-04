3f69b9191d61a5aed63f762d82d9b223ab619cab
NguyenHai7120 <nguyenhai7120qh@gmail.com>
2022-04-04 20:35:29 +0200

    Clusters

--- min_max_diff_flatten.py
+++ min_max_diff_flatten.py
@@ -1,3 +1,4 @@
+from tkinter import YView
 import numpy as np 
 
 ##############################################################################
@@ -5,7 +6,7 @@ import numpy as np
 # the Min Max Differential method applying to flattended arrays
 ##############################################################################
 
-def Encoder(X, start_by_min = True):
+def Encoder(X):
     '''
     Min Max Differential
     X : input images, of shape (N, ), if not it will be reshaped
@@ -15,14 +16,29 @@ def Encoder(X, start_by_min = True):
     '''
     N = X.shape[0]
     X = X.reshape((N, -1))
-    D = X.shape[1]
+
     min_im = np.min(X, axis=0)
     max_im = np.max(X, axis=0)
 
     Y = np.zeros(X.shape)
-    is_min = start_by_min
     Y[:, 0] = X[:,0] - min_im[0]
-    dis = (X[:,:-1] - min_im[:-1]) > (X[:,:-1] - max_im[:-1])
-    Y[:,1:] = (X[:,1:] - min_im[1:])*dis +    
+    dis = (X[:,:-1] - min_im[:-1]) < (X[:,:-1] - max_im[:-1])
+    Y[:,1:] = (X[:,1:] - min_im[1:])*dis + (max_im[1:] - X[:,1:])*(~dis)
 
+    return Y, min_im, max_im         
 
+#############################################
+
+def Decoder(Y, min_im, max_im):
+    '''
+    Min Max Differential Decoder
+    '''
+    D = Y.shape[1]
+    X = np.zeros(Y.shape)
+    X[:,0] = Y[:,0] + min_im[0]
+    
+    for d in range(1, D):
+        dis = (X[:,d-1] - min_im[d-1]) < (X[:,d-1] - max_im[d-1])
+        X[:,d] = (Y[:,d] + min_im[d])*dis + (max_im[d] - Y[:,d])*(~dis)
+
+    return X
